40b207956c385ea0bf9709e5ef7eccfecb5db1a6
ghassan-amreyah <1191704@student.birzeit.edu>
2022-11-15 21:59:30 +0200

    v1

---
+++ app/src/main/java/edu/cs1191704/calculator/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.cs1191704.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
